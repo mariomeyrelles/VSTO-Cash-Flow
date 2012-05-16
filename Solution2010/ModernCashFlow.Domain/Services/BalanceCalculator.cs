3ca5156e1a7e9b42df86d162abf079a1695cdea6
Mario Meyrelles <mariomeyrelles@yahoo.com.br>
2012-05-16 20:38:33 -0300

    Evolving balance calculation logic with some unit tests.

--- Solution2010/ModernCashFlow.Domain/Services/BalanceCalculator.cs
+++ Solution2010/ModernCashFlow.Domain/Services/BalanceCalculator.cs
@@ -7,12 +7,30 @@ namespace ModernCashFlow.Domain.Services
 {
     public class BalanceCalculatorService
     {
-        public decimal? CalculateBalance(int accountID, IEnumerable<Income> incomes, IEnumerable<Expense> expenses)
+        public decimal CalculateBalance(int accountID, IEnumerable<Income> incomes, IEnumerable<Expense> expenses)
         {
             //first try to calculate the balance.
-            var balance = incomes.Where(x => x.AccountID == accountID).Sum(x => x.ActualValue) -
-                             expenses.Where(x => x.AccountID == accountID).Sum(x => x.ActualValue);
+            var incomeSum = 0.0m;
+            var expenseSum = 0.0m;
+            foreach (var income in incomes.Where(x => x.AccountID == accountID))
+            {
+                if (income.ActualValue.HasValue)
+                    incomeSum += income.ActualValue.Value;
+                else
+                    incomeSum += income.ExpectedValue ?? 0.0m;
+            }
 
+            foreach (var expense in expenses.Where(x => x.AccountID == accountID))
+            {
+                if (expense.ActualValue.HasValue)
+                    expenseSum += expense.ActualValue.Value;
+                else
+                    expenseSum += expense.ExpectedValue ?? 0.0m;
+            }
+
+            var balance = incomeSum - expenseSum;
+
+            
             return balance;
         }
     }
--- Solution2010/ModernCashFlow.Tests/InitialTests.cs
+++ Solution2010/ModernCashFlow.Tests/InitialTests.cs
@@ -11,6 +11,7 @@ namespace ModernCashFlow.Tests
     public class InitialTests
     {
 
+        // ReSharper disable InconsistentNaming
         [Test]
         public void Can_calculate_simple_balance()
         {
@@ -65,6 +66,117 @@ namespace ModernCashFlow.Tests
 
         }
 
+        [Test]
+        public void Can_calculate_simple_balance_with_all_nulls()
+        {
+            var balanceService = new BalanceCalculatorService();
+
+            var incomes = new List<Income>();
+            incomes.Add(new Income() { AccountID = 1, ActualValue = null });
+            incomes.Add(new Income() { AccountID = 1, ActualValue = null });
+
+            var expenses = new List<Expense>();
+            expenses.Add(new Expense() { AccountID = 1, ActualValue = null });
+            expenses.Add(new Expense() { AccountID = 1, ActualValue = null });
+
+
+            var balance = balanceService.CalculateBalance(1, incomes, expenses);
+
+            Console.WriteLine("balance: " + balance);
+            Assert.IsTrue(balance == 0.0m);
+
+        }
+
+        [Test]
+        public void Can_calculate_balance_considering_actual_and_expected_values()
+        {
+            var incomes = new List<Income>();
+            incomes.Add(new Income() { AccountID = 1, ExpectedValue = 10.00m, ActualValue = 10.20m });
+            incomes.Add(new Income() { AccountID = 1, ExpectedValue = 10.01m, ActualValue = 10.21m });
+            incomes.Add(new Income() { AccountID = 1, ExpectedValue = 10.02m, ActualValue = 10.22m });
+            incomes.Add(new Income() { AccountID = 1, ExpectedValue = 10.03m, ActualValue = 10.23m });
+            incomes.Add(new Income() { AccountID = 1, ExpectedValue = 10.04m, ActualValue = 10.24m });
+            incomes.Add(new Income() { AccountID = 1, ExpectedValue = 10.05m, ActualValue = 10.25m });
+            
+            var expenses = new List<Expense>();
+            expenses.Add(new Expense() { AccountID = 1, ExpectedValue = 10.00m, ActualValue = 10.10m });
+            expenses.Add(new Expense() { AccountID = 1, ExpectedValue = 10.01m, ActualValue = 10.11m });
+            expenses.Add(new Expense() { AccountID = 1, ExpectedValue = 10.02m, ActualValue = 10.12m });
+            expenses.Add(new Expense() { AccountID = 1, ExpectedValue = 10.03m, ActualValue = 10.13m });
+            expenses.Add(new Expense() { AccountID = 1, ExpectedValue = 10.04m, ActualValue = 10.14m });
+            expenses.Add(new Expense() { AccountID = 1, ExpectedValue = 10.05m, ActualValue = 10.15m });
+
+            var balanceService = new BalanceCalculatorService();
+            var balance = balanceService.CalculateBalance(1, incomes, expenses);
+
+            Console.WriteLine("balance: " + balance);
+            Assert.IsTrue(balance == 0.60m);
+
+        }
+
        
+        [Test]
+        public void Can_calculate_balance_considering_actual_and_expected_values_and_some_nulls()
+        {
+            var incomes = new List<Income>();
+            incomes.Add(new Income() { AccountID = 1, ExpectedValue = 10.00m, ActualValue = null });
+            incomes.Add(new Income() { AccountID = 1, ExpectedValue = 10.01m, ActualValue = null });
+            incomes.Add(new Income() { AccountID = 1, ExpectedValue = 10.02m, ActualValue = 10.22m });
+            incomes.Add(new Income() { AccountID = 1, ExpectedValue = null, ActualValue = null });
+            incomes.Add(new Income() { AccountID = 1, ExpectedValue = 10.04m, ActualValue = 10.24m });
+            incomes.Add(new Income() { AccountID = 1, ExpectedValue = 10.05m, ActualValue = 10.25m });
+            
+            var expenses = new List<Expense>();
+            expenses.Add(new Expense() { AccountID = 1, ExpectedValue = null, ActualValue = 10.10m });
+            expenses.Add(new Expense() { AccountID = 1, ExpectedValue = 10.01m, ActualValue = null});
+            expenses.Add(new Expense() { AccountID = 1, ExpectedValue = 10.02m, ActualValue = 10.12m });
+            expenses.Add(new Expense() { AccountID = 1, ExpectedValue = 10.03m, ActualValue = 10.13m });
+            expenses.Add(new Expense() { AccountID = 1, ExpectedValue = null, ActualValue = 10.14m });
+            expenses.Add(new Expense() { AccountID = 1, ExpectedValue = 10.05m, ActualValue = null });
+
+            var balanceService = new BalanceCalculatorService();
+            var balance = balanceService.CalculateBalance(1, incomes, expenses);
+
+            Console.WriteLine("balance: " + balance);
+            Assert.IsTrue(balance == -9.83m);
+
+        }
+
+        [Test]
+        public void Can_calculate_balance_considering_actual_and_expected_values_some_nulls_2()
+        {
+            var incomes = new List<Income>();
+            incomes.Add(new Income() { AccountID = 1, ExpectedValue = null, ActualValue = 10.20m });
+            incomes.Add(new Income() { AccountID = 1, ExpectedValue = null, ActualValue = 10.21m });
+            incomes.Add(new Income() { AccountID = 1, ExpectedValue = null, ActualValue = 10.22m });
+            incomes.Add(new Income() { AccountID = 1, ExpectedValue = null, ActualValue = 10.23m });
+            incomes.Add(new Income() { AccountID = 1, ExpectedValue = null, ActualValue = 10.24m });
+            incomes.Add(new Income() { AccountID = 1, ExpectedValue = null, ActualValue = 10.25m });
+
+            var expenses = new List<Expense>();
+            expenses.Add(new Expense() { AccountID = 1, ExpectedValue = 10.00m, ActualValue = null });
+            expenses.Add(new Expense() { AccountID = 1, ExpectedValue = 10.01m, ActualValue = null });
+            expenses.Add(new Expense() { AccountID = 1, ExpectedValue = 10.02m, ActualValue = null });
+            expenses.Add(new Expense() { AccountID = 1, ExpectedValue = 10.03m, ActualValue = null });
+            expenses.Add(new Expense() { AccountID = 1, ExpectedValue = 10.04m, ActualValue = null });
+            expenses.Add(new Expense() { AccountID = 1, ExpectedValue = 10.05m, ActualValue = null });
+
+            var balanceService = new BalanceCalculatorService();
+            var balance = balanceService.CalculateBalance(1, incomes, expenses);
+
+            Console.WriteLine("balance: " + balance);
+            Assert.IsTrue(balance == 1.20m);
+
+        }
+        
+
+        public void Can_calculate_balance_with_initial_balance()
+        {
+        }
+
+        public void Can_calcultate_balance_from_given_date()
+        {
+        }
+
     }
 }
