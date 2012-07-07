016cd94feb7c9fa1767e40a9960b5c8864c79abd
Mario Meyrelles <mariomeyrelles@yahoo.com.br>
2012-07-07 18:13:26 -0300

    Evolving the icons and the IconTextUserControl.

---
+++ Solution2010/ModernCashFlow.WpfControls/Controls/IconTextUserControl.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ModernCashFlow.WpfControls.Controls
+{
+    /// <summary>
+    /// Interaction logic for MoneyValueControl.xaml
+    /// </summary>
+    public partial class IconTextUserControl : UserControl
+    {
+        public IconTextUserControl()
+        {
+            this.InitializeComponent();
+        }
+
+
+
+        #region Text Dependency Property
+        
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(IconTextUserControl),
+                                        new FrameworkPropertyMetadata("-",FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, ChangeText));
+
+        private static void ChangeText(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            (d as IconTextUserControl).textContent.Text = e.NewValue.ToString();
+        }
+
+        public string Text
+        {
+            get
+            {
+                return (string)GetValue(TextProperty);
+            }
+            set
+            {
+                SetValue(TextProperty, value);
+            }
+        }
+        
+        #endregion
+
+
+
+        //#region Header Text Dependency Property
+
+        //public static readonly DependencyProperty HeaderTextProperty = DependencyProperty.Register("HeaderText", typeof(string), typeof(IconTextUserControl),
+        //                                new FrameworkPropertyMetadata("-", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, ChangeHeaderText));
+
+        
+        //private static void ChangeHeaderText(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        //{
+        //    (d as IconTextUserControl).textHeader.Text = e.NewValue.ToString();
+        //}
+
+       
+        //public string HeaderText
+        //{
+        //    get
+        //    {
+        //        return (string)GetValue(HeaderTextProperty);
+        //    }
+        //    set
+        //    {
+        //        SetValue(HeaderTextProperty, value);
+        //    }
+        //}
+
+        //#endregion
+
+
+
+        #region Icon Dependency Property
+
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(object), typeof(IconTextUserControl), new FrameworkPropertyMetadata(null, ChangeIcon));
+
+        private static void ChangeIcon(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            (d as IconTextUserControl).iconControl.Content = e.NewValue;
+        }
+
+
+        public object Icon
+        {
+            get
+            {
+                return GetValue(IconProperty);
+            }
+            set
+            {
+                SetValue(IconProperty, value);
+            }
+        }
+
+        #endregion
+
+
+
+    }
+}
