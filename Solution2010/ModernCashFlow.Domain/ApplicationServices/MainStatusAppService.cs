b01de25af5f591781e18a7060ede4668df48a2fb
Mario Meyrelles <mariomeyrelles@yahoo.com.br>
2012-12-12 00:57:29 -0200

    Started preparation for view first approach on side panels.

---
+++ Solution2010/ModernCashFlow.Domain/ApplicationServices/MainStatusAppService.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+
+namespace ModernCashFlow.Domain.ApplicationServices
+{
+    public class MainStatusAppService 
+    {
+        
+        public decimal IncomesUpToDate
+        {
+            //todo: return from domain service
+            get { return 444.45m; }
+        }
+
+
+        public decimal ExpensesUpToDate
+        {
+            //todo: return from domain service
+            get { return 111.45m; }
+        }
+
+        public decimal EndOfMonthBalance
+        {
+            //todo: return from domain service
+            get { return 222.33m; }
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+      
+
+
+    }
+}
--- Solution2010/ModernCashFlow.Tests/SummaryCalculationsTDD.cs
+++ Solution2010/ModernCashFlow.Tests/SummaryCalculationsTDD.cs
@@ -10,7 +10,7 @@ namespace ModernCashFlow.Tests
     public class SummaryCalculationsTDD
     {
         [Test]
-        public void Can_Calculate_Incomes_Whole_Month()
+        public void Can_Sum_Incomes_Whole_Month()
         {
             var svc = new SummaryCalculationService();
 
@@ -39,7 +39,7 @@ namespace ModernCashFlow.Tests
         }
 
         [Test]
-        public void Can_Calculate_Incomes_Up_To_Given_Date_In_Current_Month()
+        public void Can_Sum_Incomes_Up_To_Given_Date_In_Current_Month()
         {
             var svc = new SummaryCalculationService();
             SystemTime.Now = () => new DateTime(2012,12,15);
@@ -86,14 +86,14 @@ namespace ModernCashFlow.Tests
         }
 
         [Test]
-        public void Can_Calculate_Expenses_Whole_Month()
+        public void Can_Sum_Expenses_Whole_Month()
         {
             
         }
 
     
         [Test]
-        public void Can_Calculate_Expenses_Up_To_Given_Date_In_Current_Month()
+        public void Can_Sum_Expenses_Up_To_Given_Date_In_Current_Month()
         {
             
         }
